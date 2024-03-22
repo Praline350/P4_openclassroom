28495d7460a34e32a4f5b6a315a240e39766064f
edwin thierry <edwin.thierry350@gmail.com>
2024-03-22 13:06:15 +0100

    Fusion branche main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from models import Player, DataJson, Tournament
-from controller2 import Controller
+from models import Player, Tournament
+from controller import Controller
 from view import PromptForm, Menu
 
 controller = Controller()
