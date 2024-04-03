03dc577203f65c9094eee21398fecf62794fc4de
edwin thierry <edwin.thierry350@gmail.com>
2024-04-03 16:19:24 +0200

    Remaniment de la structure de la DB

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ import random
 # Chemins vers les fichiers JSON de données
 
 JSON_DATA_PLAYERS_PATH = "data/data_players.json"
-JSON_DATA_TOURNAMENTS_PATH = "data/data_tournaments.json"
+FOLDER_DATA_TOURNAMENTS_PATH = "data/data_tournament"
 JSON_DATA_ROUNDS_PATH = "data/data_rounds.json"
 
 
@@ -22,7 +22,6 @@ class Player:
             with open(JSON_DATA_PLAYERS_PATH, "w") as f:
                 json.dump([], f)
         # Initialisation de la requête pour les joueurs
-        self.PlayerQuery = Query()
 
     def write_player(self, surname, name, birth_date, national_id):
         # Écriture des données d'un joueur dans la base de données
@@ -36,219 +35,208 @@ class Player:
 
     def find_player(self, national_id):
         # Recherche d'un joueur par son ID national
-        player_data = self.players.search(self.PlayerQuery.national_id == national_id)
+        player_data = self.players.search(Query().national_id == national_id)
         if player_data:
             return player_data[0]
         else:
             return None
+
+    def get_all_player_id(self):
+        player_data = self.players.all()
+        sorted_id = sorted(player_data, key=lambda x: x["national_id"])
+        players_ids = []
+        for ids in sorted_id:
+            players_ids.append(ids["national_id"])
+        return players_ids
+
+    def get_all_player_name(self):
+        player_name = []
+        player_data = self.players.all()
+        sorted_name = sorted(player_data, key=lambda x: x["name"])
+        for name in sorted_name:
+            player_name.append(name["name"])
+        return player_name
 
 
 class Tournament:
 
     def __init__(self):
         self.player = Player()
-        # Initialisation de la base de données des tournois
-        self.db_tournament = TinyDB(JSON_DATA_TOURNAMENTS_PATH, indent=4)
-        if not os.path.exists(JSON_DATA_TOURNAMENTS_PATH):
-            with open(JSON_DATA_TOURNAMENTS_PATH, "w") as f:
-                json.dump([], f)
+        # Initialisation du folder data_tournament
+        if not os.path.exists(FOLDER_DATA_TOURNAMENTS_PATH):
+            os.makedirs(FOLDER_DATA_TOURNAMENTS_PATH)
         # Initialisation des requêtes pour les joueurs et les tournois
-        self.PlayerQuery = Query()
-        self.TournamentQuery = Query()
+
+    def initialize_db(self, name_tournament):
+        file_path = f"data/data_tournament/{name_tournament}.json"
+        self.db_tournament = TinyDB(file_path, indent=4)
+        self.tournament = self.db_tournament.table(name_tournament)
 
     def write_tournament(
         self, name_tournament, localisation, round, start_date, end_date
     ):
-        # Écriture des données d'un tournoi dans la base de données
-        self.tournaments = self.db_tournament.table(name_tournament)
+        self.initialize_db(name_tournament)
         self.data = {
             "name_tournament": name_tournament,
             "localisation": localisation,
             "rounds_number": round,
-            "rounds": [],
-            "actual_round": 0,
             "start_date": start_date,
             "end_date": end_date,
+            "actual_round": 0,
             "player_list": [],
             "description": "",
         }
-        self.tournaments.insert(self.data)
+        self.tournament.insert(self.data)
 
     def remove_tournament(self, name_tournament):
-        if self.db_tournament.table(name_tournament):
-            # Supprimer la table (ou le tournoi)
-            self.db_tournament.drop_table(name_tournament)
+        file_path = f"data/data_tournament/{name_tournament}.json"
+        if os.path.exists(file_path):
+            os.remove(file_path)
             print("Tournoi supprimé avec succès.")
         else:
             print("Tournoi inexistant.")
 
     def add_player(self, name_tournament, id_player):
-        # Ajout d'un joueur à un tournoi
-        self.name_tournament = name_tournament
-        self.id_player = id_player
-        tournament_table = self.db_tournament.table(name_tournament)
         player_data = self.player.find_player(id_player)
         if player_data:
-            player_name = player_data["name"]
-            national_id = player_data["national_id"]
+            player_in_tournament = self.find_player_in_tournament(
+                name_tournament, id_player
+            )
             tournament_data = self.find_tournament(name_tournament)
             if tournament_data:
                 player_list = tournament_data.get("player_list", [])
-                player_in_tournament = any(
-                    player.get("national_id") == id_player for player in player_list
-                )
                 if not player_in_tournament:
                     player_list.append(
-                        {"national_id": national_id, "name": player_name, "score": 0}
+                        {
+                            "national_id": player_data["national_id"],
+                            "name": player_data["name"],
+                            "score": 0,
+                        }
                     )
-                    tournament_table.update(
+                    self.tournament.update(
                         {"player_list": player_list},
-                        self.TournamentQuery.name_tournament == name_tournament,
+                        Query().name_tournament == name_tournament,
                     )
-                    print(f"Joueur {player_name} ajouté")
+                    print(f"Joueur {player_data['name']} ajouté à {name_tournament}")
                 else:
-                    print("Joueur déjà dans le tournoi")
+                    print(f"{player_data['name']} déjà dans {name_tournament}")
+            else:
+                print("tournoi inexistant")
         else:
-            print("tournoi inexistant")
+            print("Joueur inéxistant")
 
     def remove_player(self, name_tournament, id_player):
-        tournament_table = self.db_tournament.table(name_tournament)
-        tournament_data = tournament_table.get(
-            Query().name_tournament == name_tournament
+        player_in_tournament = self.find_player_in_tournament(
+            name_tournament, id_player
         )
+        tournament_data = self.find_tournament(name_tournament)
+        if tournament_data:
+            player_list = tournament_data.get("player_list", [])
+        if player_in_tournament:
+            for player in player_list:
+                if player.get("national_id") == id_player:
+                    player_list.remove(player)
+                    print(f"Joueur {player['name']} retiré du {name_tournament}")
+                    break
+            self.tournament.update(
+                {"player_list": player_list},
+                Query().name_tournament == name_tournament,
+            )
 
+    def find_tournament(self, name_tournament):
+        file_path = f"data/data_tournament/{name_tournament}.json"
+        self.db_tournament = TinyDB(file_path, indent=4)
+        tournament_table = self.db_tournament.table(name_tournament)
+        tournament_data = tournament_table.all()
+        if tournament_data:
+            return tournament_data[0]
+        else:
+            return None
+
+    def find_player_in_tournament(self, name_tournament, id_player):
+        self.initialize_db(name_tournament)
+        tournament_data = self.find_tournament(name_tournament)
         if tournament_data:
             player_list = tournament_data.get("player_list", [])
             player_in_tournament = any(
                 player["national_id"] == id_player for player in player_list
             )
-
-            if player_in_tournament:
-                updated_player_list = [
-                    player
-                    for player in player_list
-                    if player["national_id"] != id_player
-                ]
-                tournament_table.update(
-                    {"player_list": updated_player_list},
-                    Query().name_tournament == name_tournament,
-                )
-                print(
-                    f"Le joueur {id_player} a été retiré du tournoi {name_tournament}."
-                )
-            else:
-                print(
-                    f"Le joueur {id_player} n'est pas dans le tournoi {name_tournament}."
-                )
-        else:
-            print(f"Impossible de trouver le tournoi {name_tournament}.")
-
-    def find_tournament(self, name_tournament):
-        # Recherche d'un tournoi par son nom
-        tournament_table = self.db_tournament.table(name_tournament)
-        tournament_data = tournament_table.all()
-        if tournament_data:
-            return tournament_data[0]  # Retourne le premier tournoi trouvé
-        else:
-            return None
+            # retourne True si le joueur est dans le tournoi
+            return player_in_tournament
 
     def get_name_tournaments(self):
-        # Récupération de la liste des noms de tournois
-        return self.db_tournament.tables()
+        names_tournament = []
+        for filename in os.listdir(FOLDER_DATA_TOURNAMENTS_PATH):
+            name_tournament = os.path.splitext(filename)[0]
+            names_tournament.append(name_tournament)
+        return names_tournament
 
 
 class Round:
 
     def __init__(self):
         self.tournament = Tournament()
-        self.db_rounds = TinyDB(JSON_DATA_ROUNDS_PATH, indent=4)
-
-        if not os.path.exists(JSON_DATA_ROUNDS_PATH):
-            with open(JSON_DATA_ROUNDS_PATH, "w") as f:
-                json.dump([], f)
-        self.RoundsQuery = Query()
-        self.TournamentQuery = Query()
+        self.player = Player()
 
     def add_round(self, name_tournament):
-        self.rounds = self.db_rounds.table(name_tournament)
-
-        # Initialisation des dates automatiques
+        self.tournament.initialize_db(name_tournament)
+        round_table = self.tournament.db_tournament.table("rounds")
         start_date = datetime.now()
         end_date = start_date + timedelta(hours=4)
-        tournament_table = self.tournament.db_tournament.table(name_tournament)
-        tournament_data = self.tournament.find_tournament(name_tournament)
-        if tournament_data:
-            tournament_round = tournament_data.get("rounds", [])
-            round_index = len(self.rounds.all()) + 1
-            actual_round = +1
-            self.data = {
-                "round_index": round_index,
-                "start_date": start_date.strftime("%H:%M"),
-                "end_date": end_date.strftime("%H:%M"),
-                "game_list": [],
-            }
-            self.rounds.insert(self.data)
-            tournament_round.append(self.data)
-            tournament_table.update(
-                {"rounds": tournament_round, "actual_round": actual_round},
-                self.TournamentQuery.name_tournament == name_tournament,
-            )
-            return round_index
-        else:
-            print("tournoi inexistant")
+        len_round = len(round_table)
+        round_index = len_round + 1
+        round_data = {
+            "round_index": round_index,
+            "start_date": start_date.strftime("%H:%M"),
+            "end_date": end_date.strftime("%H:%M"),
+            "game_list": []
+        }
+        round_table.insert(round_data)
 
-    def find_round(self, round_index, name_tournament):
-        # Trouve un round grace a son index de round et le nom du tournoi auquel il appartient
-        round_table = self.db_rounds.table(name_tournament)
-        rounds = round_table.all()
-        for round_data in rounds:
-            if round_data.get("round_index") == round_index:
-                return round_data
-        return None
+        return round_index
 
     def mix_players_random(self, name_tournament):
-        # Melange les joeurs présent dans le tournoi
+        self.tournament.initialize_db(name_tournament)
         tournament_data = self.tournament.find_tournament(name_tournament)
         if tournament_data:
             tournament_round = tournament_data.get("player_list", [])
             random.shuffle(tournament_round)
-            self.tournament.tournaments.update(
+            self.tournament.tournament.update(
                 {"player_list": tournament_round},
-                self.TournamentQuery.name_tournament == name_tournament,
+                Query().name_tournament == name_tournament,
             )
 
-    def add_game(self, game):
-        pass
+    def find_round(self, name_tournament, round_index):
+        self.tournament.initialize_db(name_tournament)
+        round_table = self.tournament.db_tournament.table("rounds")
+        round_data = round_table.get(Query().round_index == round_index)
+        if round_data and round_table:
+            return round_data
 
 
 class Game:
+
     def __init__(self):
         self.round = Round()
         self.tournament = Tournament()
         self.player = Player()
-        self.RoundQuery = Query()
-        self.PlayerQuery = Query()
 
     def make_game(self, name_tournament, round_index):
-        # Créer les matchs en fonctions des joueurs présent dans le tournoi pour le round en cours
-        self.round_index = round_index
-        round_table = self.round.db_rounds.table(name_tournament)
-        round_data = self.round.find_round(round_index, name_tournament)
+        self.tournament.initialize_db(name_tournament)
         tournament_data = self.tournament.find_tournament(name_tournament)
-        if tournament_data:
-            # Recupère les joueurs présent dans le tournoi
+        round_table = self.tournament.db_tournament.table('rounds')
+        round_data = self.round.find_round(name_tournament, round_index)
+        if tournament_data and round_data:
+            game_list = round_data.get("game_list", [])
             player_list = tournament_data.get("player_list", [])
             num_players = len(player_list)
             # Vérifie que les joueurs sont bien un nombre pair
             if num_players % 2 != 0:
                 print("nombre impaire")
                 return
-            game_id = 1
-            # Créer les pairs de joueurs
             for i in range(0, num_players, 2):
                 player_pair = (player_list[i], player_list[i + 1])
-                game_list = round_data.get("game_list", [])
-                # Verifie que le joueur n'est pas déjà dans un match
+                game_id = len(game_list) + 1
                 pair_already_in_game = False
                 for game in game_list:
                     if (
@@ -259,12 +247,13 @@ class Game:
                         break
                 # Si les joueurs ne sont pas encore dans un match, créer un match par pair
                 if not pair_already_in_game:
-                    game_data = {"game_id": game_id, "players": player_pair}
-                    game_list.append(game_data)
-                    round_table.update({"game_list": game_list}, doc_ids=[round_index])
-                    game_id += 1
-                else:
-                    print("Un joueur est déjà présent dans la game_list")
+                    game_list.append({"game_id": game_id, "players": player_pair})
+            print(game_list)
+            round_table.update({'game_list': game_list}, Query().round_index == round_index)
+                    
+            
+
+            # self.tournament.tournament.update({'rounds': game_list}, Query().name_tournament == name_tournament)
 
     def play_game(self, name_tournament, round_index):
         round_table = self.round.db_rounds.table(name_tournament)
